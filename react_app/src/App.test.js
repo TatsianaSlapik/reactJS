85121b01d2ca78d0cfc509da54100dc69abfa35e
Tatiana <Slapik>
2023-03-27 01:38:37 +0300

    install Cypress and add tests

--- react_app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react h1", () => {
-  /* render(<App />);
-  const h1Element = screen.getByText("FIND YOUR MOViE");
-  expect(h1Element).toBeInTheDocument();*/
-});
